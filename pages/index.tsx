581e284e70b31740f82444ff340eb0f2e92d1aa6
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-03-01 20:30:54 +0900

    Delete cloned file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import Link from 'next/link'
-import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <div><h1>Coloca</h1><h2>A flexible SNS to communicate, share, cocentrate with your groups.</h2></div>
 )
 
 export default IndexPage
